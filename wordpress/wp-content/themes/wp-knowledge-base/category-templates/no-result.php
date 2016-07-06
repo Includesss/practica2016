08853230cbea9264e3919a6c2f4d7c91422dcff3
costmc <costmc@me.com>
2016-07-06 13:57:01 +0300

    Plugin added

--- wordpress/wp-content/themes/wp-knowledge-base/category-templates/no-result.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package iPanelThemes Knowledgebase
- */
-?>
-<div class="list-group-item text-muted">
-	<span class="glyphicon ipt-icon-blocked"></span> <?php _e( 'No articles published. Check back later?', 'ipt_kb' ); ?>
-</div>
